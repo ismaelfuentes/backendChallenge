b88ceead228e1487af2b0ad6af65bb7123ef9b44
root <root@ismael>
2023-12-10 19:54:15 +0100

    remove built

--- dist/src/common/errors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ERRORS = void 0;
-var ERRORS;
-(function (ERRORS) {
-    ERRORS["EMAIL_IN_USE"] = "EMAIL_IN_USE";
-})(ERRORS || (exports.ERRORS = ERRORS = {}));
